0a8d76e9016bdb4ebafff23a4fb01aa7d745fa1a
Son <ptson117@gmail.com>
2018-01-29 14:57:59 +0700

    zz

--- model.py
+++ model.py
@@ -1,19 +1,19 @@
 from tf_unet import unet, util, image_util
 
 #preparing data loading
-data_provider = image_util.ImageDataProvider(data_suffix=u"./train/AOI_3_Paris/*.tif",mask_suffix=u"./train/AOI_3_Paris/*_mask.tif")
+data_provider = image_util.ImageDataProvider(data_suffix=u"outpath/resized/*.tif",mask_suffix=u"outpath/mask/*_mask.tif")
 
 #setup & training
-net = unet.Unet(layers=3, features_root=64, channels=1, n_class=2)
+output_path = "./out_path/model"
+net = unet.Unet(layers=3, features_root=64, channels=3, n_class=2)
 trainer = unet.Trainer(net)
-path = trainer.train(data_provider, output_path, training_iters=32, epochs=100)
-
+path = trainer.train(data_provider, output_path, training_iters=32, epochs=10, restore=True, dropout=0.75, display_step=2,)
+print("done")
 #verification
-...
+# data = 
+# prediction = net.predict(path, data)
+# print(prediction)
+# unet.error_rate(prediction, util.crop_to_shape(label, prediction.shape))
 
-prediction = net.predict(path, data)
-
-unet.error_rate(prediction, util.crop_to_shape(label, prediction.shape))
-
-img = util.combine_img_prediction(data, label, prediction)
-util.save_image(img, "prediction.jpg")
+# img = util.combine_img_prediction(data, label, prediction)
+# util.save_image(img, "prediction.jpg")
