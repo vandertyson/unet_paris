73a844f8a0d99e1f916e60bb824ec11f1e76b78a
vandertyson <vandertyson117@gmail.com>
2018-02-27 13:13:39 +0700

    Add files via upload

---
+++ im_pre_1.py
@@ -0,0 +1,97 @@
+
+
+import skimage.transform
+import shapely.wkt
+#import tables as tb
+import tqdm
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import gdal
+from os import listdir
+from os.path import isfile, join
+import scipy
+
+
+FN_MASK_CSV = "/data/train/AOI_2_Vegas_Train/summaryData/AOI_2_Vegas_Train_Building_Solutions.csv"
+#FN_MASK_CSV = "./AOI_3_Paris_Train_Building_Solutions.csv"
+#FN_RGB = "./out_path/RGB/"
+FN_RGB = "/data/train/AOI_2_Vegas_Train/RGB-PanSharpen/"
+INPUT_SIZE = 256
+
+def resize_original_im(img):
+    data3 = img.copy()
+    bandstats = {k: dict(max=0, min=0) for k in range(3)}
+    for i in range(3):
+        bandstats[i]['min'] = scipy.percentile(data3[i], 2)
+        bandstats[i]['max'] = scipy.percentile(data3[i], 98)
+
+    for chan_i in range(3):
+        min_val = bandstats[chan_i]['min']
+        max_val = bandstats[chan_i]['max']
+        data3[chan_i] = np.clip(data3[chan_i], min_val, max_val)
+        data3[chan_i] = (data3[chan_i] - min_val)/(max_val- min_val)*255
+    img2 = np.array(data3).swapaxes(0,1).swapaxes(1,2)    
+    return img2
+
+def prepare_mask_tif():
+    df_summary = pd.read_csv(FN_MASK_CSV)
+    df = pd.read_csv(FN_MASK_CSV, usecols=[0]).iloc[:]
+    ids = [f[:-4] for f in listdir(FN_RGB) if isfile(join(FN_RGB, f))]
+    for image_id in ids[:]:        
+        mask = image_mask_resized_from_summary(df_summary, image_id[15:])
+       # plt.imshow(mask)
+       # plt.show()
+        im_path = FN_RGB + image_id + ".tif"
+        # im_path = "./RGB-PanSharpen_AOI_3_Paris_img197.tif"
+        out_path1 = "./out_path/mask/" + image_id + "_mask.tif"
+        # im_path = "/data/train/AOI_3_Paris/RGB_PanSharpen_" + image_id + ".tif"
+
+
+        ds = gdal.Open(im_path)
+        driver = gdal.GetDriverByName("GTiff")
+        ## get resized mask
+        dst_ds1 = driver.Create(out_path1, 650, 650, (1), gdal.GDT_Byte)                          
+        dst_ds1.GetRasterBand(1).WriteArray(mask)
+        dst_ds1.GetRasterBand(1).ComputeStatistics(False)
+        dst_ds1.SetProjection(ds.GetProjection())
+        dst_ds1.SetGeoTransform(ds.GetGeoTransform())
+
+        ## get resized img
+        out_path2 = "./out_path/resized/" + image_id + ".tif"
+        img2 = ds.ReadAsArray()
+        img2 = resize_original_im(img2)
+        dst_ds2 = driver.Create(out_path2,650,650,3,gdal.GDT_Byte)
+        for i in range(1,img2.shape[2]+1):
+            dst_ds2.GetRasterBand(i).WriteArray(img2[:,:,i-1])
+            dst_ds2.GetRasterBand(i).ComputeStatistics(False)
+        dst_ds2.SetProjection(ds.GetProjection())
+        dst_ds2.SetGeoTransform(ds.GetGeoTransform())
+
+def image_mask_resized_from_summary(df, image_id):
+    print(image_id)
+    im_mask = np.zeros((650, 650))
+    for idx, row in df[df.ImageId == image_id].iterrows():
+        shape_obj = shapely.wkt.loads(row.PolygonWKT_Pix)
+        #print image_id
+        #print shape_obj
+        if shape_obj.exterior is not None:
+            coords = list(shape_obj.exterior.coords)
+            x = [round(float(pp[0])) for pp in coords]
+            y = [round(float(pp[1])) for pp in coords]
+            yy, xx = skimage.draw.polygon(y, x, (650, 650))
+            im_mask[yy, xx] = 1
+
+            interiors = shape_obj.interiors
+            for interior in interiors:
+                coords = list(interior.coords)
+                x = [round(float(pp[0])) for pp in coords]
+                y = [round(float(pp[1])) for pp in coords]
+                yy, xx = skimage.draw.polygon(y, x, (650, 650))
+                im_mask[yy, xx] = 0
+    # im_mask = skimage.transform.resize(im_mask, (INPUT_SIZE, INPUT_SIZE))
+    # im_mask = (im_mask > 0.5).astype(np.uint8)
+    return im_mask
+
+
+prepare_mask_tif()
